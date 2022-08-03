f9d3851d68b595d1e12b4874d5aab5b94b85b152
timikus2004 <timikus2004@gmail.com>
2022-08-03 13:12:01 +0300

    initial commit to gihub

--- start.py
+++ start.py
@@ -9,7 +9,7 @@ from werkzeug.security import check_password_hash,generate_password_hash
 
 app = Flask(__name__)
 
-ENV = 'prod'
+ENV = 'dev'
 
 if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://timikus:password@localhost/myapp'
