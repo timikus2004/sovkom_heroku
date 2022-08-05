756d1907bdf3b27a2018ce8abb384155606760ca
timikus2004 <timikus2004@gmail.com>
2022-08-05 18:59:49 +0300

    new deploy

--- start.py
+++ start.py
@@ -15,7 +15,7 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://timikus:password@localhost/myapp'
     app.debug = True
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lchvtkoztfyvnt:9f1ff8fe29d951cb27a821bcee40882863a224b3636b316f47a769b4d264852c@ec2-3-213-228-206.compute-1.amazonaws.com:5432/d3ann4ba97q4s9'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rvkvqakqvuuths:0dbef51e9fac3b9c8278d561e66a14cbfa51b2c566127cd99d425917f528291b@ec2-44-205-112-253.compute-1.amazonaws.com:5432/d4u1t5hsfvs1ll'
     app.debug = False
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
